08589151eee60a8935247b86997fcd6e0754ee75
Thomas Nguyen <tomxnguy@gmail.com>
2023-09-19 04:20:00 +0000

    Added data.js javascript. #1.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+
+window.addEventListener('beforeunload', serializeData);
+function serializeData(event) {
+  const newData = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', newData);
+}
+
+const checkStorage = localStorage.getItem('javascript-local-storage');
+if (checkStorage !== null) {
+  data = JSON.parse(checkStorage);
+}
