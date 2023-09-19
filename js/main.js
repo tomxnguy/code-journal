f4fdf197869ed297f2d5ba58df16cd6631936db2
Thomas Nguyen <tomxnguy@gmail.com>
2023-09-19 02:40:30 +0000

    Add submit event listener. #1

--- js/main.js
+++ js/main.js
@@ -5,3 +5,24 @@ $urlInput.addEventListener('input', newPhoto);
 function newPhoto(event) {
   $photo.setAttribute('src', event.target.value);
 }
+
+const $formValue = document.querySelector('#entry-form');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const title = $formValue.elements.title.value;
+  const url = $formValue.elements.url.value;
+  const notes = $formValue.elements.notes.value;
+  const noteData = {
+    entryId: data.nextEntryId,
+    title,
+    url,
+    notes,
+  };
+  data.nextEntryId++;
+  data.entries.unshift(noteData);
+  $photo.setAttribute('src', '/images/placeholder-image-square.jpg');
+  $formValue.reset();
+}
+
+$formValue.addEventListener('submit', handleSubmit);
