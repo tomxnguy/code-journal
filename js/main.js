92ff7db8f9354d851702264587bd8a3c9153890d
Thomas Nguyen <tomxnguy@gmail.com>
2023-09-20 17:43:26 +0000

    DOMContentLoaded event listener added. #2.

--- js/main.js
+++ js/main.js
@@ -26,3 +26,38 @@ function handleSubmit(event) {
 }
 
 $formValue.addEventListener('submit', handleSubmit);
+
+function renderEntry(entry) {
+  const $listDom = document.createElement('li');
+  $listDom.setAttribute('class', 'row');
+
+  const $imgDiv = document.createElement('div');
+  $imgDiv.setAttribute('class', 'column-half');
+  $listDom.appendChild($imgDiv);
+
+  const $imgRender = document.createElement('img');
+  $imgRender.setAttribute('src', entry.url);
+  $imgDiv.appendChild($imgRender);
+
+  const $textDivRender = document.createElement('div');
+  $textDivRender.setAttribute('class', 'column-half');
+  $listDom.appendChild($textDivRender);
+
+  const $titleRender = document.createElement('h3');
+  $titleRender.textContent = entry.title;
+  $textDivRender.appendChild($titleRender);
+
+  const $noteRender = document.createElement('p');
+  $noteRender.textContent = entry.notes;
+  $textDivRender.appendChild($noteRender);
+
+  return $listDom;
+}
+
+const $unOrderedList = document.querySelector('ul');
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const $newEntry = renderEntry(data.entries[i]);
+    $unOrderedList.append($newEntry);
+  }
+});
